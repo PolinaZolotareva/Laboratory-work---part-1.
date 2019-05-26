9d608797c3657839ebca612a80e9dd349651a74b
PolinaZolotareva <47950881+PolinaZolotareva@users.noreply.github.com>
2019-05-27 02:30:09 +0300

    Delete Zadanie_1.py

--- Zadanie_1.py
+++
@@ -1,7 +0,0 @@
-string = 'Привет, Лимон, как, твое, Лицо'
-string = string.split(', ')
-for i in range(0, len(string)):
-    if string[i][0]== "Л" and string[i][1]=='и':
-        print(string[i])
-
-
